0b5943490de5e90ff9a0b5cd9d6608603d680adf
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-06 17:57:51 +0300

    Add more selectors to the "phones" module

--- src/re-ducks/modules/phones/selectors.js
+++ src/re-ducks/modules/phones/selectors.js
@@ -1,5 +1,47 @@
-const selectPhones = state => state.phones;
+import { createSelector } from 'reselect';
+
+const selectPhones = state => (
+  state && 
+  state.phones && 
+  state.phones
+) || {};
+
+
+const selectItems = createSelector(
+  selectPhones,
+  phones => (phones.items && phones.items) || []
+);
+
+const selectPhone = (state, props) => 
+  selectItems(state).find(item => item.id === parseInt(props.match.params.id, 10));
+
+const selectCompanies = createSelector(
+  selectItems,
+  items => [...new Set(items.map(item => item.company))]
+);
+
+const selectPrices = createSelector(
+  selectItems,
+  items => [...new Set(items.map(item => item.price))]
+);
+
+const selectPriceRange = createSelector(
+  selectPrices,
+  prices => {
+    const sortedPrices = prices.slice().sort((a, b) => a - b);
+
+    return {
+      min: sortedPrices[0],
+      max: sortedPrices[sortedPrices.length - 1]
+    };
+  }
+);
 
 export default {
+  selectPhone,
   selectPhones,
+  selectItems,
+  selectCompanies,
+  selectPrices,
+  selectPriceRange
 };
