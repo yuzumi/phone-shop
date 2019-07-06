3d7f35512f0ca5a08aeeaeeee0c137ee1418dcf0
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-06 18:00:37 +0300

    Combine cart reducer

--- src/re-ducks/reducer.js
+++ src/re-ducks/reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import cartReducer from 're-ducks/modules/cart';
 import phonesReducer from 're-ducks/modules/phones';
 
 const reducer = combineReducers({
+  cart: cartReducer,
   phones: phonesReducer,
 });
 
