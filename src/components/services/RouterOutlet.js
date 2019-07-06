eebbb2ac27e56675e8d4ddf21ea662490819062c
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-06 11:08:54 +0300

    Add RouterOutlet component

---
+++ src/components/services/RouterOutlet.js
@@ -0,0 +1,27 @@
+import React, { Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const RouterOutlet = ({ routes, fallback = null }) => {
+  const renderRoute = route => <Route key={route.path} {...route} />;
+
+  return (
+    <Suspense fallback={fallback}>
+      <Switch>{routes.map(renderRoute)}</Switch>
+    </Suspense>
+  );
+};
+
+RouterOutlet.propTypes = {
+  routes: PropTypes.arrayOf(
+    PropTypes.shape({
+      path: PropTypes.string.isRequired,
+      exact: PropTypes.bool,
+      render: PropTypes.func,
+      component: PropTypes.node
+    })
+  ).isRequired,
+  fallback: PropTypes.node
+};
+
+export default RouterOutlet;
