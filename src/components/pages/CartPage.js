ef902dd12fe256fd872165d133dd38120817c151
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-06 11:07:53 +0300

    Add blank pages

---
+++ src/components/pages/CartPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CartPage = () => (
+  <section></section>
+);
+
+export default CartPage;
---
+++ src/components/pages/ProductPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductPage = () => (
+    <section></section>
+);
+
+export default ProductPage;
