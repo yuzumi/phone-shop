acef682929a057a58cbb8ea2e518f839cef33244
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-06 11:09:29 +0300

    Import ErrorBoundary component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 import App from 'components/App';
+import ErrorBoundary from 'components/services/ErrorBoundary';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
@@ -11,9 +12,11 @@ import 'mdbreact/dist/css/mdb.css';
 import 'index.css';
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ErrorBoundary>
+    <Router>
+      <App />
+    </Router>
+  </ErrorBoundary>,
   document.getElementById('root')
 );
 
