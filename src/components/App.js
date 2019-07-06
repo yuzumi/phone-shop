b76c6454daaa0504abdc51e5d39cd651847796fd
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-06 11:10:10 +0300

    Configure routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import Template from 'components/layout/Template';
+import RouterOutlet from 'components/services/RouterOutlet';
+
+import routes from 'routes';
+
 const App = () => (
   <div className="app">
-    
+    <Template>
+      <RouterOutlet {...{
+          routes
+        }} 
+      />
+    </Template>
   </div>
 );
 
