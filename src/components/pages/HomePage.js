7d718b083a1ac26a7550ddb85de67cfe0a89cbd8
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-06 13:20:49 +0300

    Apply PhoneList component

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { MDBRow, MDBCol } from 'mdbreact';
+
+import PhoneList from 'components/phone/PhoneList';
 
 const HomePage = () => (
-  <section></section>
+  <section className="page home">
+    <MDBRow>
+      <MDBCol md="3"></MDBCol>
+      <MDBCol md="9">
+        <PhoneList />
+      </MDBCol>
+    </MDBRow>
+  </section>
 );
 
 export default HomePage;
