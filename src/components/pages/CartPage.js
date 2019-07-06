fae140cce48259f16a465385cd96d7bcd5edef70
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-06 18:10:29 +0300

    Update CartPage component

--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { selectors, actions } from 're-ducks/modules/cart';
+import { MDBAlert } from 'mdbreact';
+import CartList from 'components/cart/CartList';
+import CartTotal from 'components/cart/CartTotal';
 
-const CartPage = () => (
-  <section></section>
+const CartPage = ({ items, total, removeFromCart }) => (
+  <section className="page cart">
+    {(items.length > 0) ? (
+      <>
+        <CartList 
+          {...{
+            items,
+            removeFromCart
+          }}
+        />
+        <CartTotal total={total} />
+      </>
+    ) : (
+      <MDBAlert className="text-center" color="warning">
+        Cart is empty
+      </MDBAlert>
+    )}
+  </section>
 );
 
-export default CartPage;
+const mapStateToProps = state => ({
+  items: selectors.selectItems(state),
+  total: selectors.selectTotal(state),
+});
+
+export default connect(
+  mapStateToProps,
+  { removeFromCart: actions.removeFromCart }
+)(CartPage);
