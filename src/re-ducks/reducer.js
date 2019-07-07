5f2f18afea00f6098de2e3f06da032bb83f31d44
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-07 10:32:35 +0300

    Combine view reducer

--- src/re-ducks/reducer.js
+++ src/re-ducks/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import cartReducer from 're-ducks/modules/cart';
+import viewReducer from 're-ducks/modules/view';
 import phonesReducer from 're-ducks/modules/phones';
 
 const reducer = combineReducers({
   cart: cartReducer,
+  view: viewReducer,
   phones: phonesReducer,
 });
 
