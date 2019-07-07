f2b2834d55b5d9b991a88817a46f00ece1c0bb9b
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-07 11:51:23 +0300

    Add more cart selectors

--- src/re-ducks/modules/cart/selectors.js
+++ src/re-ducks/modules/cart/selectors.js
@@ -20,8 +20,23 @@ const selectTotal = createSelector(
   items => items.reduce((total, item) => total + item.price, 0)
 );
 
+const selectItemsCount = createSelector(
+  selectItems,
+  items => items.length
+);
+
+const isInCart = state => {
+  const cartItems = selectItems(state);
+
+  return item => {
+    return cartItems.findIndex(cartItem => cartItem.id === item.id) !== -1;
+  };
+};
+
 export default {
+  isInCart,
   selectCart,
   selectItems,
   selectTotal,
+  selectItemsCount,
 };
