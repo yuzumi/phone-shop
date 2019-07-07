fce393207c84e27d2eec44340efe4e6bfd93a19c
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-07 10:36:10 +0300

    Connect the component to the redux store

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import { MDBRow, MDBCol } from 'mdbreact';
+import Spinner from 'components/shared/Spinner';
+import { connect } from 'react-redux';
+import { selectors as phonesSelectors } from 're-ducks/modules/phones';
+import { selectors as viewSelectors } from 're-ducks/modules/view';
+import { actions } from 're-ducks/modules/cart';
 
-import PhoneGrid from 'components/phone/PhoneGrid';
+import PhoneView from 'components/phone/PhoneView';
+import ControlBar from 'components/control/ControlBar';
 
-const HomePage = () => (
+const HomePage = ({ isLoading, ...restProps }) => (
   <section className="page home">
-    <MDBRow>
-      <MDBCol md="3"></MDBCol>
-      <MDBCol md="9">
-        <PhoneGrid />
-      </MDBCol>
-    </MDBRow>
+    {isLoading ? (
+      <Spinner />
+    ) : (
+        <>
+          <MDBRow>
+            <MDBCol>
+              <ControlBar />
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol md="3"></MDBCol>
+            <MDBCol md="9">
+              <PhoneView {...restProps} />
+            </MDBCol>
+          </MDBRow>
+        </>
+      )}
   </section>
 );
 
-export default HomePage;
+const mapStateToProps = state => ({
+  ...phonesSelectors.selectPhones(state),
+  view: viewSelectors.selectView(state),
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    addToCart: actions.addToCart,
+    removeFromCart: actions.removeFromCart,
+  }
+)(HomePage);
