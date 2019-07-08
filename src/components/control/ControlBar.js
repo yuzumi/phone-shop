8480822b4087491870b3fc3f8a695f10c6702964
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-08 21:52:24 +0300

    Move the functionality of selecting the view option into a separate component

--- src/components/control/ControlBar.js
+++ src/components/control/ControlBar.js
@@ -1,43 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { actions, selectors, constants } from 're-ducks/modules/view';
-import { MDBCard, MDBCardBody, MDBBtn, MDBIcon } from 'mdbreact';
+import { MDBCard, MDBCardBody } from 'mdbreact';
+import ViewSwitch from 'components/control/ViewSwitch';
 
-const viewOptions = {
-  [constants.GRID]: {
-    view: constants.GRID,
-    icon: 'th',
-  },
-  [constants.LIST]: {
-    view: constants.LIST,
-    icon: 'th-list',
-  }
-};
-
-const ControlBar = ({ view, setView }) => (
+const ControlBar = () => (
   <MDBCard>
     <MDBCardBody>
       <div className="d-flex align-items-center justify-content-end">
-        {Object.entries(viewOptions).map(([ key, value ]) => (
-          <MDBBtn
-            outline={value.view === view} 
-            size="sm" 
-            onClick={() => setView(value.view)} 
-            key={key}
-          >
-            <MDBIcon icon={value.icon} size="lg" />
-          </MDBBtn>
-        ))}
+        <ViewSwitch />
       </div>
     </MDBCardBody>
   </MDBCard>
 );
 
-const mapStateToProps = state => ({
-  view: selectors.selectView(state),
-});
-
-export default connect(
-  mapStateToProps,
-  { setView: actions.setView }
-)(ControlBar);
+export default ControlBar;
