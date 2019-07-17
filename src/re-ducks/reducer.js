8c315f3b384b1793d29a77aadd6528e1a797e12a
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-17 13:58:44 +0300

    Combine sorting reducer

--- src/re-ducks/reducer.js
+++ src/re-ducks/reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import cartReducer from 're-ducks/modules/cart';
 import viewReducer from 're-ducks/modules/view';
 import phonesReducer from 're-ducks/modules/phones';
+import sortingReducer from 're-ducks/modules/sorting';
 
 const reducer = combineReducers({
   cart: cartReducer,
   view: viewReducer,
   phones: phonesReducer,
+  sorting: sortingReducer,
 });
 
 export default reducer;
