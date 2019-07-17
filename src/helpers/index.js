d834cfdf8dee0eeaf5ff17504d2aec0543232af4
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-17 14:00:19 +0300

    Add helper functions to compare values

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,13 @@ export const wait = delayInMs =>
 
 export const parseDecimal = value => 
   parseInt(value, 10);
+
+export const compare = (a, b) => {
+  if (a < b) return -1;
+
+  if (a > b) return 1;
+
+  return 0;
+}
+
+export const compareNumbers = (a, b) => a - b;
